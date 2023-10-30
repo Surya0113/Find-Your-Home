335c884aad75a8b99645c132b24c7a5276a36e1f
Surya pratap yadav <98934699+Surya0113@users.noreply.github.com>
2023-10-30 17:45:47 +0530

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,23 @@
+<div class="footer">
+    <div class="page-container footer-container">
+        <div class="footer-cities">
+            <div class="footer-city">
+                <a href="property_list.php?city=gida">PG in gida</a>
+            </div>
+            <div class="footer-city">
+                <a href="property_list.php?city=gorakhpur">PG in gorakhpur</a>
+            </div>
+            <div class="footer-city">
+                <a href="property_list.php?city=kushinagar">PG in kushinagar</a>
+            </div>
+            <div class="footer-city">
+                <a href="property_list.php?city=sahjanwa">PG in sahjanwa</a>
+            </div>
+        </div>
+        <div class="footer-copyright">© 2020 Copyright PG Life </div>
+    </div>
+</div>
+
+<script type="text/javascript" src="js/jquery.js"></script>
+<script type="text/javascript" src="js/bootstrap.min.js"></script>
+<script type="text/javascript" src="js/common.js"></script>
